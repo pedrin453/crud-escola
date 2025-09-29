53dc7c16979169c38e9b8a334feff99a9ce3ea79
pedrin453 <pedro.francisco@iesb.edu.br>
2025-09-29 20:40:52 -0300

    feat: configuração inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const alunosRoutes = require('./routes/alunos');
+const professoresRoutes = require('./routes/professores');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/alunos', alunosRoutes);         // Membro 1
+
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
